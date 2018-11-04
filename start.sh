081cedd89f24ca96095536be334b6687f0058d5f
mrclemrkz <awesomeisme007@gmail.com>
2018-11-05 00:39:41 +0530

    DefaultRealm changed.

--- start.sh
+++ start.sh
@@ -2,17 +2,17 @@
 
 docker-compose up --build -d
 
-STATUS="0"
+# STATUS="0"
 
-until [[ ${STATUS} == *"ok"* ]]; do
-    echo "   wating until slpd is started"
-    STATUS=`docker exec -ti copper-openldap sh -c "service slapd status"`
-    sleep 5
-done
+# until [[ ${STATUS} == *"ok"* ]]; do
+#     echo "   wating until slpd is started"
+#     STATUS=`docker exec -ti copper-openldap sh -c "service slapd status"`
+#     sleep 5
+# done
 
-echo "   slapd has started!"
+# echo "   slapd has started!"
 
-# sleep 30
+sleep 20
 
 docker exec -it copper-openldap /usr/bin/ldapadd -Y EXTERNAL -H ldapi:// -f /home/97-wso2Person.ldif
 docker exec -it copper-openldap /usr/bin/ldapadd -Y EXTERNAL -H ldapi:// -f /home/98-scimPerson.ldif
